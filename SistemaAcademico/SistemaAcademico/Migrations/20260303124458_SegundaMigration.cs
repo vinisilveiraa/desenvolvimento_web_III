3d3eca03bde511772195ee5ac9e1440fed9a5607
Vinicius Leonardo Silveira <vinicius.silveir1503@gmail.com>
2026-03-03 11:43:29 -0300

    aula 03-03

---
+++ SistemaAcademico/SistemaAcademico/Migrations/20260303124458_SegundaMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaAcademico.Migrations
+{
+    /// <inheritdoc />
+    public partial class SegundaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Matricula_AlunoId",
+                table: "Matricula",
+                column: "AlunoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matricula_CursoId",
+                table: "Matricula",
+                column: "CursoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matricula_Alunos_AlunoId",
+                table: "Matricula",
+                column: "AlunoId",
+                principalTable: "Alunos",
+                principalColumn: "AlunoId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matricula_Cursos_CursoId",
+                table: "Matricula",
+                column: "CursoId",
+                principalTable: "Cursos",
+                principalColumn: "CursoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matricula_Alunos_AlunoId",
+                table: "Matricula");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matricula_Cursos_CursoId",
+                table: "Matricula");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matricula_AlunoId",
+                table: "Matricula");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matricula_CursoId",
+                table: "Matricula");
+        }
+    }
+}
---
+++ SistemaAcademico/SistemaAcademico/Models/Matricula.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaAcademico.Models
+{
+    public class Matricula
+    {
+        public int MatriculaId { get; set; }
+        public DateTime DataMatricula { get; set; }
+
+        public int AlunoId { get; set; }
+        public Aluno? Aluno { get; set; }
+        public int CursoId { get; set; }
+        public Curso? Curso { get; set; }
+    }
+}
